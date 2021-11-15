284052b1e374c741e503d855c0ee2311c521fbc0
soumendrak <proud_odia@outlook.com>
2021-11-15 07:31:54 +0530

    intermediate commit

--- scrapeProject/spiders/dharitri_spider.py
+++ scrapeProject/spiders/dharitri_spider.py
@@ -1,4 +1,11 @@
+import datetime
+import socket
+
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose
+
+from scrapeProject.items import ScrapeprojectItem
 
 
 class ArticleLinksSpider(scrapy.Spider):
@@ -7,7 +14,26 @@ class ArticleLinksSpider(scrapy.Spider):
 
     def parse(self, response):
         for article in response.css("div.caption"):
-            yield {
-                "article_link": article.css("a.text-black").attrib['href'],
-            }
+            yield from article.follow_all(css="a.text-black", callback=self.parse_articles)
         yield from response.follow_all(css="a.page.larger", callback=self.parse)
+
+    def parse_articles(self, response):
+        # header = response.xpath("//header[@class='cono']//h1/text()").extract()
+        # [text.strip() for text in header if text.strip()]
+        # article_text: List[str] = response.xpath("//div[@class='p-t-20']/p//text()").extract()
+        l = ItemLoader(item=ScrapeprojectItem(), response=response)
+
+        # Load fields using XPath expressions
+        l.add_xpath('header', "//header[@class='cono']//h1/text()",
+                    MapCompose(unicode.strip))
+        l.add_xpath('content', "//div[@class='p-t-20']/p//text()",
+                    MapCompose(unicode.strip))
+
+        # Housekeeping fields
+        l.add_value('url', response.url)
+        l.add_value('project', self.settings.get('BOT_NAME'))
+        l.add_value('spider', self.name)
+        l.add_value('server', socket.gethostname())
+        l.add_value('date', datetime.datetime.now())
+
+        return l.load_item()
