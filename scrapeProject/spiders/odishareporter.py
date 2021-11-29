5f265c27746cc7a59d43e0533551ab5c367f7278
soumendrak <proud_odia@outlook.com>
2021-11-29 13:21:16 +0530

    MINOR CHANGES

--- scrapeProject/spiders/odishareporter.py
+++ scrapeProject/spiders/odishareporter.py
@@ -6,22 +6,21 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapeProject.items import ScrapeprojectItem
 
 
-class DharitriSpider(CrawlSpider):
-    name = 'dharitri'
-    start_urls = [f"https://www.dharitri.com/{year}/{month:02}" for year in range(2019, 2022) for month in range(1, 13)]
+class OdishareporterSpider(CrawlSpider):
+    name = 'odishareporter'
+    start_urls = [f"https://odishareporter.in/post-sitemap{lot}.xml" for lot in range(1, 150)]
 
     # Rules for horizontal and vertical scrolling
     rules = (
-        Rule(LinkExtractor(restrict_xpaths="//a[@class='nextpostslink']"), follow=True),
-        Rule(LinkExtractor(restrict_css="a.text-black"), follow=True, callback='parse_item'),
+        Rule(LinkExtractor(restrict_xpaths="//table[@id='sitemap']/tbody/tr/td"), follow=True, callback='parse_item'),
     )
 
     def parse_item(self, response):
         l = ItemLoader(item=ScrapeprojectItem(), response=response)
 
         # Load fields using XPath expressions
-        l.add_xpath('header', "//header[@class='cono']//h1/text()",
+        l.add_xpath('header', "//h1[@class='single-post-title']//text()",
                     MapCompose(lambda text: text.strip()), Join())
-        l.add_xpath('content', "//div[@class='p-t-20']/p//text()",
+        l.add_xpath('content', "//div[@class='entry-content clearfix single-post-content']/p//text()",
                     MapCompose(lambda text: text.strip()), Join())
         return l.load_item()
